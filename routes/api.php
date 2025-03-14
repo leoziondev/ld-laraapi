112b49167187bad263a5af1257df76f39c4a0f5d
leoziondev <leozionsuporte@gmail.com>
2025-03-14 20:57:04 -0300

    feat: add category controller show metod

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('categories', [CategoryController::class, 'index']);
+Route::get('categories/{category}', [CategoryController::class, 'show']);
