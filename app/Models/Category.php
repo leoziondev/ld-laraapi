7ce24e4407f216e403561d362cea79f2e2b16035
leoziondev <leozionsuporte@gmail.com>
2025-03-14 21:23:49 -0300

    feat: add description in category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,5 +10,5 @@ class Category extends Model
     /** @use HasFactory<\Database\Factories\CategoryFactory> */
     use HasFactory;
 
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'description'];
 }
