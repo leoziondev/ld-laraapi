9adc205a64f67d5d8fe7836ed12327b321dd5909
leoziondev <leozionsuporte@gmail.com>
2025-03-14 21:45:38 -0300

    feat: add product mf

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -22,5 +23,6 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         Category::factory(10)->create();
+        Product::factory(20)->create();
     }
 }
